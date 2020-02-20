54955aa20c42215dbb5708bce4ed5f9cc8787a4b
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-02-20 10:29:01 +0900

    Updates

---
+++ precache-manifest.67f75877741d59e0539380edf4a26a00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0859ce276f79d18af3bccf900675cb",
+    "url": "/envelope-craftsman-js/index.html"
+  },
+  {
+    "revision": "8bb93f09389b44dbff4d",
+    "url": "/envelope-craftsman-js/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d417179b43d91434f043",
+    "url": "/envelope-craftsman-js/static/js/2.4b173902.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/envelope-craftsman-js/static/js/2.4b173902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bb93f09389b44dbff4d",
+    "url": "/envelope-craftsman-js/static/js/main.b0e00495.chunk.js"
+  },
+  {
+    "revision": "3bc88ac9a5540caed7d5",
+    "url": "/envelope-craftsman-js/static/js/runtime-main.4382ad36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/envelope-craftsman-js/static/media/logo.5d5d9eef.svg"
+  }
+]);
