261396ac364663e284122bcbcc887a4e88e733e1
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-03-03 13:27:09 +0900

    アプリバーの下に固定表示するレスポンシブドロワー。 ウィンドウの幅が特定のサイズ（xsDown）より小さくなると、PCメニューを非表示にし、モバイルメニューを表示します。

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Button } from '@material-ui/core';
 import PaperPatternGenForm from './components/PaperPatternGenForm';
 import Envelope from './components/Envelope';
+import ResponsiveDrawer from './components/ResponsiveDrawer';
 
 function App() {
   const [isPreview, setIsPreview] = React.useState(false);
@@ -24,15 +25,16 @@ function App() {
   }
 
   return (
-    <PaperPatternGenForm onSubmit={(values, { setSubmitting }) => {
-      setTimeout(() => {
-        setSubmitting(false);
-        setIsPreview(true)
-        console.log(JSON.stringify(values, null, 2));
-      }, 500);
-    }} />
+    <ResponsiveDrawer container={
+      <PaperPatternGenForm onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          setSubmitting(false);
+          setIsPreview(true)
+          console.log(JSON.stringify(values, null, 2));
+        }, 500);
+      }} />
+    }/>
   )
-
 }
 
 export default App;
